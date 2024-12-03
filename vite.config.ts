d6e45e3baec74f982a81f48652533d0114a87232
deadmau5v <deadmau5v@qq.com>
2024-12-03 10:28:45 +0800

    fix: 修复 /v/* 子页面不能用 使用 serve

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,22 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  appType: 'spa',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
 })
