c13dcd73f39bc786dc06d7f02c2c59397f7d2e6c
d5v <deadmau5v@qq.com>
2025-07-11 23:44:10 +0800

    feat: 更新RWPP版本信息，修正发布日期并添加Windows下载链接

--- src/data/gameVersions.ts
+++ src/data/gameVersions.ts
@@ -56,14 +56,15 @@ export const gameVersions: GameVersion[] = [
     },
   },
   {
-    version: "RWPP 1.5.3.2",
+    version: "RWPP 1.5.3",
     beta: false,
     thirdParty: true,
-    releaseDate: "2025-6-30",
+    releaseDate: "2025-7-11",
     description:
       "现代化的第三方版本，拥有更现代化的UI，更多开房选项，以及更稳定的游戏体验。",
     downloads: {
       Android: "https://file-02.d5v.cc/d/public/pe/RWPP1.5.3.2.apk",
+      Windows: "https://file-02.d5v.cc/d/public/pc/RWPP1.5.3.exe",
       Github: "*https://github.com/Minxyzgo/RWPP",
     },
   },
